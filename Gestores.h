1e65fa569d4ff7e1b587580699448b977343dac4
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2023-02-28 23:26:39 +0000

    .

---
+++ Gestores.h
@@ -0,0 +1,7 @@
+#pragma once
+
+typedef struct {
+	char nome[50];
+	char email[50];
+	char password[20];
+} Gestor;
---
+++ Main.c
@@ -0,0 +1,16 @@
+/*****************************************************************//**
+ * \file   Main.c
+ * \brief  Main
+ * 
+ * \author A. Cerqueira
+ * \date   February 2023
+ *********************************************************************/
+
+#include <stdio.h>
+
+
+int main()
+{
+	// say hello
+	printf("Hello World!");
+}
