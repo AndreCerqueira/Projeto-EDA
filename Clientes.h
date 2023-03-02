535feafea3c2feca314a004e8335a5277b140287
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2023-03-02 14:17:16 +0000

    .

--- Clientes.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef struct {
-	char nome[50];
-	char nif[20];
-	char morada[50];
-	float saldo;
-} Cliente;
--- Gestores.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef struct {
-	char nome[50];
-	char email[50];
-	char password[20];
-} Gestor;
